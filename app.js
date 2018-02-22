285eb51bb8b95267bebfdaacab5834083bcb14c8
99BestBuy <36514343+99BestBuy@users.noreply.github.com>
2018-02-22 15:40:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ myEl.css('display','none');
   });
 
 
-app.controller('DetailsController', ['$routeParams','$http', function($routeParams,$http) {
+app.controller('DetailsController', ['$routeParams','$http','$sce', function($routeParams,$http,$sce) {
  this.pid = $routeParams.pid;
  this.type = $routeParams.type;
  this.memoryCardsData = sd;
@@ -26,240 +26,7 @@ app.controller('DetailsController', ['$routeParams','$http', function($routePara
  this.powerBanksData = pb;
  this.speakersData = sp;
  this.imageSource = "1";
- this.showPaymentButton = function(id){
- 	console.log("displaying button for "+id);
- 	var div = angular.element( document.querySelector( '#paymentButton' ) );
-	div.css('display','block');
-	var evo8 = angular.element( document.querySelector( '#evo8' ) );
-	var evo16 = angular.element( document.querySelector( '#evo16' ) );
-	var evo32 = angular.element( document.querySelector( '#evo32' ) );
-	var evo64 = angular.element( document.querySelector( '#evo64' ) );
-	var san8 = angular.element( document.querySelector( '#san8' ) );
-	var san16 = angular.element( document.querySelector( '#san16' ) );
-	var san32 = angular.element( document.querySelector( '#san32' ) );
-	var san64 = angular.element( document.querySelector( '#san64' ) );
-	var pd8 = angular.element( document.querySelector( '#pd8' ) );
-	var pd16 = angular.element( document.querySelector( '#pd16' ) );
-	var pd32 = angular.element( document.querySelector( '#pd32' ) );
-	var pd64 = angular.element( document.querySelector( '#pd64' ) );
-	var rs699 = angular.element( document.querySelector( '#rs699' ) );
-	if(id=='evo8'){
-		evo8.css('display','block');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
-		rs699.css('display','none');
-	};
- 	if(id=='evo16'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','block');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id=='evo32'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');		
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','block');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id=='evo64'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','block');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id=='san8'){
- 		rs699.css('display','none');
- 		san8.css('display','block');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id=='san16'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','block');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id=='san32'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','block');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
-	};
- 	if(id=='san64'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','block');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
-	};
- 	if(id=='sanpd8'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','block');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
-	};
- 	if(id=='sanpd16'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','block');
-		pd32.css('display','none');
-		pd64.css('display','none');
-	};
- 	if(id=='sanpd32'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','block');
-		pd64.css('display','none');
-	};
- 	if(id=='sanpd64'){
- 		rs699.css('display','none');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','block');
-	};
- 	if(id==='mi20000'){
- 		rs699.css('display','block');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- 	if(id==='jbl_go'){
- 		rs699.css('display','block');
- 		san8.css('display','none');
-		san16.css('display','none');
-		san32.css('display','none');
-		san64.css('display','none');
-		evo8.css('display','none');
-		evo16.css('display','none');
-		evo32.css('display','none');
-		evo64.css('display','none');
-		pd8.css('display','none');
-		pd16.css('display','none');
-		pd32.css('display','none');
-		pd64.css('display','none');
- 	};
- };
- // this.showPaymentButton(this.pid);
- this.hidePaymentButton = function(){
- 	console.log("hide function called");
- 	var myEl = angular.element( document.querySelector( '#paymentButton' ) );
-	myEl.css('display','none');
- };
+
  this.getType = function(){
  	if (this.type=='sd') {return this.memoryCardsData};
  	if (this.type=='pd') {return this.usbDrivesData};
@@ -320,7 +87,8 @@ var sd = [
 			"evo32",
 			"evo64",
 			"evo128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/3b4fsq"
 	},
 	{
 		"id":"evo16",
@@ -340,7 +108,8 @@ var sd = [
 			"evo32",
 			"evo64",
 			"evo128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/W7Dnq3"
 	},
 	{
 		"id":"evo32",
@@ -360,7 +129,8 @@ var sd = [
 			"evo16",
 			"evo64",
 			"evo128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/d4VTdW"
 	},{
 		"id":"evo64",
 		"name":"Samsung Evo 64gb class 10 memory card",
@@ -379,7 +149,8 @@ var sd = [
 			"evo16",
 			"evo32",
 			"evo128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/KJwHv9"
 	},{
 		"id":"evo128",
 		"name":"Samsung Evo 128gb class 10 memory card",
@@ -398,7 +169,8 @@ var sd = [
 			"evo16",
 			"evo32",
 			"evo64"
-		]
+		],
+		"paymentUrl":""
 	},{
 		"id":"san8",
 		"name":"Sandisk 8gb class 10 memory card",
@@ -417,7 +189,8 @@ var sd = [
 			"san32",
 			"san64",
 			"san128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/hdn82X"
 	},{
 		"id":"san16",
 		"name":"Sandisk 16gb class 10 memory card",
@@ -436,7 +209,8 @@ var sd = [
 			"san32",
 			"san64",
 			"san128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/PJRxk2"
 	},{
 		"id":"san32",
 		"name":"Sandisk 32gb class 10 memory card",
@@ -455,7 +229,8 @@ var sd = [
 			"san16",
 			"san64",
 			"san128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/4GSZsX"
 	},
 	{
 		"id":"san64",
@@ -475,7 +250,8 @@ var sd = [
 			"san16",
 			"san32",
 			"san128"
-		]
+		],
+		"paymentUrl":"https://imjo.in/WnHczk"
 	},
 	{
 		"id":"san128",
@@ -495,7 +271,8 @@ var sd = [
 			"san16",
 			"san32",
 			"san64",
-		]
+		],
+		"paymentUrl":""
 	}
 ];
 
@@ -517,7 +294,8 @@ var pd = [
 			"sanpd16",
 			"sanpd32",
 			"sanpd64",
-		]
+		],
+		"paymentUrl":"https://imjo.in/Bzkf3J"
 	},
 	{
 		"id":"sanpd16",
@@ -536,7 +314,8 @@ var pd = [
 			"sanpd8",
 			"sanpd32",
 			"sanpd64"
-		]
+		],
+		"paymentUrl":"https://imjo.in/52KkyW"
 	},
 	{
 		"id":"sanpd32",
@@ -555,7 +334,8 @@ var pd = [
 			"sanpd8",
 			"sanpd16",
 			"sanpd64"
-		]
+		],
+		"paymentUrl":"https://imjo.in/yemqSp"
 	},{
 		"id":"sanpd64",
 		"name":"Sandisk Cruzer Blade 64 GB",
@@ -573,7 +353,8 @@ var pd = [
 			"sanpd8",
 			"sanpd16",
 			"sanpd32"
-		]
+		],
+		"paymentUrl":"https://imjo.in/p7Nhwt"
 	}
 ];
 
@@ -594,7 +375,8 @@ var pb = [
 		"description":"",
 		"similarItems":[
 			""
-		]
+		],
+		"paymentUrl":"https://imjo.in/fFpHDn"
 	}
 ];
 
@@ -614,6 +396,7 @@ var sp = [
 		"description":"If you unexpectedly happen to end up at a house party on a Friday night and your friends don't have any sound system to play tunes on, this speaker will be a saviour and take the party on all night long.",
 		"similarItems":[
 			""
-		]
+		],
+		"paymentUrl":"https://imjo.in/fFpHDn"
 	}
 ];
